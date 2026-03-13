5ae714f79e130bbbe71b23eb471e2236849964c2
Shadyteal2 <vanshsyadav44@gmail.com>
2026-03-14 00:59:12 +0530

    chore(build): Add build and settings configurations

--- gradlew.bat
+++ gradlew.bat
@@ -38,6 +38,9 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
+@rem FIX: Override GRADLE_USER_HOME to project directory to bypass permission errors
+set GRADLE_USER_HOME=%APP_HOME%\.gradle-home
+
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
